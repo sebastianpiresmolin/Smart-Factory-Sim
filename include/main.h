d107f0ef182d4840f361206a3aad166cc325c0f7
sebastianpiresmolin <piresmolinsecure@gmail.com>
2025-05-08 13:05:54 +0200

    Initialize project

---
+++ include/main.h
@@ -0,0 +1,16 @@
+﻿//
+// Created by Sebastian Molin on 2025-05-08.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "Smart Factory Simulator starting..." << std::endl;
+    return 0;
+}
